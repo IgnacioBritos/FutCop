baa9da4039ed586104e7f060600e9bda844eab39
GasparBritos <gasparbritos2011@gmail.com>
2024-09-03 21:56:26 -0300

    add models

--- src/controllers/reservations.controllers.js
+++ src/controllers/reservations.controllers.js
@@ -3,14 +3,14 @@ import { Reservation } from "../models/Reservations.js"
 
 export const createReservation = async (req, res) => {
     try {
-        const { user_id , date, time, number_of_players, location,description,start_date,
+        const { user_id, date, time, number_of_players, location, description, start_date,
             end_date } = req.body
         const newReservation = await Reservation.create({
             start_date,
             end_date,
-            date, 
+            date,
             time,
-            number_of_players, 
+            number_of_players,
             location,
             description,
             user_id,
@@ -18,7 +18,7 @@ export const createReservation = async (req, res) => {
         })
         res.json(newReservation)
     } catch (error) {
-        return res.status(500).json({message:error.message})
+        return res.status(500).json({ message: error.message })
     }
 }
 
@@ -27,6 +27,48 @@ export const getReservation = async (req, res) => {
     try {
         const allReservation = await Reservation.findAll()
         res.json(allReservation)
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
+
+export const getOneReservation = async (req, res) => {
+    try {
+        const { id } = req.params
+        const reservation = await Reservation.findOne({
+            where: {
+                id
+            }
+        })
+        res.json(reservation)
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
+
+export const deleteReservation = async (req,res)=>{
+    try {
+        const {id}= req.params
+        await Users.destroy({
+            where:{
+                id,
+            }
+        })
+        res.sendStatus(204)
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
+
+export const updateReservations = async (req,res) =>{
+    try {
+        const {id} = req.params
+        const { status} = req.body
+        const reservation = await Reservation.findByPk(id)
+        reservation.status= status
+        await reservation.save()
+        res.json(reservation)
     } catch (error) {
         return res.status(500).json({ message: error.message })
     }
