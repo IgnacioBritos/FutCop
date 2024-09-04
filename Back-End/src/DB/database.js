2e79f539bab8f76bea495cdcac6a662c9b8bbfa5
GasparBritos <gasparbritos2011@gmail.com>
2024-09-04 14:31:42 -0300

    fix database

---
+++ Back-End/src/DB/database.js
@@ -0,0 +1,10 @@
+import Sequelize from "sequelize";
+
+
+const sequelize = new Sequelize('FutCopDB', 'postgres', 'gaspar', {
+    host: 'localhost',
+    dialect: 'postgres',
+
+})
+
+export default sequelize;
