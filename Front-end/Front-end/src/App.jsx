d5ad8a91706ca048348ef0c8aa343649e533aeec
GasparBritos <gasparbritos2011@gmail.com>
2024-09-06 17:57:55 -0300

    add css

--- Front-end/Front-end/src/App.jsx
+++ Front-end/Front-end/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import PublicPage from './components/Auth/LoginPage/PublicPage.jsx';
+import Register from "./components/Auth/Register.jsx";
 function App() {
   return (
-    <div className=" min-h-screen bg-fixed top-0 left-0 z-[-2]   bg-[#040a11] '">
+    <div className=" min-h-screen bg-fixed top-0 left-0 z-[-2] bg-[#040a11]">
 
   <Routes>
     <Route path="/" element={<PublicPage/>} />
+    <Route path="/register" element={<Register/>}/>
   </Routes>
 
     </div>
