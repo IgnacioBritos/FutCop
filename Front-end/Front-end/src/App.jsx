5c040dc479e74aeb532b09543a1ee34f9dfc21af
unknown <IgnacioBritos>
2024-10-28 14:29:29 -0300

    fix

--- Front-end/Front-end/src/App.jsx
+++ Front-end/Front-end/src/App.jsx
@@ -1,24 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-import PublicPage from './components/Auth/LoginPage/PublicPage.jsx';
-import Register from "./components/Auth/Register.jsx";
+import PublicPage from "./components/Auth/LoginPage/PublicPage.jsx";
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
+import Home from "./components/Home/Home.jsx";
 function App() {
-  const isAuthenticated = useSelector(state=> state.user.isAuthenticated)
-  
-  
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+
   return (
     <div className=" min-h-screen bg-fixed top-0 left-0 z-[-2] bg-[#040a11]">
-
-  <Routes>
-    {
-      isAuthenticated ? <Route path="/" element={<Home/>}/>: ""
-      }
-    <Route path="/" element={<PublicPage/>} />
-  </Routes>
-
+      <main>
+        <Routes>
+          {isAuthenticated ? <Route path="/" element={<Home />} /> : ""}
+          <Route path="/" element={<PublicPage />} />
+        </Routes>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
