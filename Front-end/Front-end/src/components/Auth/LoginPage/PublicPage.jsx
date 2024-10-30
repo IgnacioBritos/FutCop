e71f6caae92e29b99630584a81e4f23251531e5f
unknown <IgnacioBritos>
2024-10-30 13:20:50 -0300

    fix the components

--- Front-end/Front-end/src/components/Auth/LoginPage/PublicPage.jsx
+++ Front-end/Front-end/src/components/Auth/LoginPage/PublicPage.jsx
@@ -25,12 +25,12 @@ const PublicPage = () => {
         <div className="w-1/2 p-6 pr-4 border-r border-[#ffffff38]">
           <h1 className="text-4xl font-bold">FutCop</h1>
           <p className="mt-4">
-            FutCop es una plataforma diseñada para facilitar la reserva de
-            canchas de fútbol. Permitiendo que el equipo indique cuántos
-            jugadores tienen disponibles y las fechas en las que desean jugar.
-            La aplicación ofrece una experiencia sencilla para gestionar las
-            reservas de manera eficiente, conectando equipos con clubes y
-            asegurando que encuentren el mejor horario para sus partidos.
+            FutCop es la solución perfecta para equipos que buscan nuevos
+            desafíos en la cancha. Conecta con otros equipos listos para jugar,
+            sin complicaciones ni largas coordinaciones. Organiza tus partidos
+            en segundos, encuentra rivales variados y asegura una experiencia
+            emocionante en cada encuentro. ¡Forma tu equipo, encuentra un rival
+            y vive el fútbol como nunca antes!
           </p>
         </div>
         {/* Segunda columna (lado derecho) */}
